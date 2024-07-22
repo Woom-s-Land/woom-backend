f5f435b1e8250289d92b2d919186845527cb9c30
이현수 <znight1020@naver.com>
2024-07-22 21:04:09 +0900

    Feat : access-token, refresh-token 유효기간 설정

--- src/main/java/com/ee06/wooms/global/jwt/JWTUtil.java
+++ src/main/java/com/ee06/wooms/global/jwt/JWTUtil.java
@@ -23,8 +23,8 @@ public class JWTUtil {
                    @Value("${spring.jwt.refresh-token-validity-in-seconds}") Long refreshTokenValidityInMilliseconds
     ) {
         this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
-        this.accessTokenValidityInMilliseconds = accessTokenValidityInMilliseconds;
-        this.refreshTokenValidityInMilliseconds = refreshTokenValidityInMilliseconds;
+        this.accessTokenValidityInMilliseconds = accessTokenValidityInMilliseconds * 1000;
+        this.refreshTokenValidityInMilliseconds = refreshTokenValidityInMilliseconds * 1000;
     }
 
     public String getUserName(String token){
