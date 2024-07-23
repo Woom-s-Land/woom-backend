df1005b529916be7e2d473d435e3b600e43d55ea
이현수 <znight1020@naver.com>
2024-07-23 16:35:00 +0900

    Feat : 회원가입, 로그인 DTO 생성

---
+++ src/main/java/com/ee06/wooms/domain/users/dto/auth/Login.java
@@ -0,0 +1,15 @@
+package com.ee06.wooms.domain.users.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class Login {
+    private String email;
+    private String password;
+}
