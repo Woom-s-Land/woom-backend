bb489289300e33f29d9238ccd776c045b9f445c9
홍성우 <sungwoo166@gmail.com>
2024-07-29 18:10:31 +0900

    Feat : 그룹 참여 기능 구현 (#41)

--- src/main/java/com/ee06/wooms/domain/enrollments/repository/EnrollmentRepository.java
+++ src/main/java/com/ee06/wooms/domain/enrollments/repository/EnrollmentRepository.java
@@ -1,11 +1,17 @@
 package com.ee06.wooms.domain.enrollments.repository;
 
 import com.ee06.wooms.domain.enrollments.entity.Enrollment;
+
+import com.ee06.wooms.domain.users.entity.User;
+import com.ee06.wooms.domain.wooms.entity.Wooms;
 import java.util.List;
+import java.util.Optional;
+
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Enrollment.Pk> {
     List<Enrollment> findByUserUuid(UUID userUuid);
 
+    Optional<Enrollment> findByUserAndWooms(User user, Wooms wooms);
 }
