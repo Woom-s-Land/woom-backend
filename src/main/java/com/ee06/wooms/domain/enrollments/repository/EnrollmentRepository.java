9908b7d185b02e80435174b372f0e4f380b69271
hennible0612 <sungwoo166@gmail.com>
2024-07-30 00:06:15 +0900

    Feat : UserInfo, WoomDetailInfo Dto 구현

--- src/main/java/com/ee06/wooms/domain/enrollments/repository/EnrollmentRepository.java
+++ src/main/java/com/ee06/wooms/domain/enrollments/repository/EnrollmentRepository.java
@@ -1,17 +1,21 @@
 package com.ee06.wooms.domain.enrollments.repository;
 
 import com.ee06.wooms.domain.enrollments.entity.Enrollment;
-
+import com.ee06.wooms.domain.enrollments.entity.EnrollmentStatus;
 import com.ee06.wooms.domain.users.entity.User;
 import com.ee06.wooms.domain.wooms.entity.Wooms;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
-
 import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Enrollment.Pk> {
     List<Enrollment> findByUserUuid(UUID userUuid);
 
     Optional<Enrollment> findByUserAndWooms(User user, Wooms wooms);
+
+    Optional<Enrollment> findByPkUserUuidAndWoomsId(UUID userUuid, Long woomsId);
+
+    List<Enrollment> findByPkWoomIdAndStatus(Long woomId, EnrollmentStatus status);
 }
