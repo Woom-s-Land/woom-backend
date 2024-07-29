dcc52f5278cf5928f6fff5d7c32985e76a7f0ff7
hennible0612 <sungwoo166@gmail.com>
2024-07-30 00:06:49 +0900

    Feat : 그룹 정보 조회 로직 구현

--- src/main/java/com/ee06/wooms/domain/wooms/controller/WoomsController.java
+++ src/main/java/com/ee06/wooms/domain/wooms/controller/WoomsController.java
@@ -3,21 +3,18 @@ package com.ee06.wooms.domain.wooms.controller;
 
 import com.ee06.wooms.domain.users.dto.CustomUserDetails;
 import com.ee06.wooms.domain.wooms.dto.WoomsCreateRequestDto;
+import com.ee06.wooms.domain.wooms.dto.WoomsDetailInfoDto;
 import com.ee06.wooms.domain.wooms.dto.WoomsDto;
 import com.ee06.wooms.domain.wooms.service.WoomsService;
 import com.ee06.wooms.global.common.CommonResponse;
-import java.util.List;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -34,15 +31,19 @@ public class WoomsController {
     }
 
     @GetMapping("/wooms")
-    public ResponseEntity<List<WoomsDto>> getWoomsInfo(@AuthenticationPrincipal CustomUserDetails currentUser) {
-        List<WoomsDto> woomsInfo = woomsService.findAllWoomsByUser(UUID.fromString(currentUser.getUuid()));
+    public ResponseEntity<List<WoomsDto>> getWoomsInfo( @AuthenticationPrincipal CustomUserDetails currentUser) {
+        List<WoomsDto> woomsInfo = woomsService.findAllWooms(UUID.fromString(currentUser.getUuid()));
         return ResponseEntity.ok(woomsInfo);
     }
 
-
     @PostMapping("/wooms/{woomsInviteCode}/users")
-    public ResponseEntity<CommonResponse> woomsParticipationRequest(@AuthenticationPrincipal CustomUserDetails currentUser, @PathVariable("woomsInviteCode") String woomsInviteCode) {
+    public ResponseEntity<CommonResponse> woomsParticipationRequest(@AuthenticationPrincipal CustomUserDetails currentUser,
+            @PathVariable("woomsInviteCode") String woomsInviteCode) {
         return ResponseEntity.ok(woomsService.createWoomsParticipationRequest(currentUser, woomsInviteCode));
+    }
 
+    @GetMapping("/wooms/{woomsId}/info")
+    public ResponseEntity<WoomsDetailInfoDto> getWoomsDetailInfo(@AuthenticationPrincipal CustomUserDetails currentUser,  @PathVariable("woomsId") Long woomsId) {
+        return ResponseEntity.ok(woomsService.findWoomsDetail(currentUser, woomsId));
     }
 }
--- src/main/java/com/ee06/wooms/domain/wooms/service/WoomsService.java
+++ src/main/java/com/ee06/wooms/domain/wooms/service/WoomsService.java
@@ -4,25 +4,27 @@ import com.ee06.wooms.domain.enrollments.entity.Enrollment;
 import com.ee06.wooms.domain.enrollments.entity.EnrollmentStatus;
 import com.ee06.wooms.domain.enrollments.repository.EnrollmentRepository;
 import com.ee06.wooms.domain.users.dto.CustomUserDetails;
+import com.ee06.wooms.domain.users.dto.UserInfoDto;
 import com.ee06.wooms.domain.users.entity.User;
 import com.ee06.wooms.domain.users.exception.ex.UserNotFoundException;
 import com.ee06.wooms.domain.users.repository.UserRepository;
 import com.ee06.wooms.domain.wooms.dto.WoomsCreateRequestDto;
+import com.ee06.wooms.domain.wooms.dto.WoomsDetailInfoDto;
 import com.ee06.wooms.domain.wooms.dto.WoomsDto;
 import com.ee06.wooms.domain.wooms.entity.Wooms;
-
 import com.ee06.wooms.domain.wooms.exception.ex.WoomsAlreadyMemberException;
 import com.ee06.wooms.domain.wooms.exception.ex.WoomsAlreadyWaitingException;
 import com.ee06.wooms.domain.wooms.exception.ex.WoomsNotValidInviteCodeException;
-
+import com.ee06.wooms.domain.wooms.exception.ex.WoomsUserNotEnrolledException;
 import com.ee06.wooms.domain.wooms.repository.WoomsRepository;
 import com.ee06.wooms.global.common.CommonResponse;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Transactional
@@ -50,13 +52,8 @@ public class WoomsService {
         return woomsRepository.save(newWooms);
     }
 
-    private User fetchUser(String userUuidStr) {
-        UUID userUuid = UUID.fromString(userUuidStr);
-        return userRepository.findById(userUuid)
-                .orElseThrow(UserNotFoundException::new);
-    }
 
-    public List<WoomsDto> findAllWoomsByUser(UUID userUuid) {
+    public List<WoomsDto> findAllWooms(UUID userUuid) {
         List<Wooms> wooms = woomsRepository.findByUserUuid(userUuid);
         return wooms.stream()
                 .map(Wooms::toDto)
@@ -86,6 +83,34 @@ public class WoomsService {
         if (enrollment.getStatus() == EnrollmentStatus.ACCEPT) {
             throw new WoomsAlreadyMemberException();
         }
+    }
+
+    public WoomsDetailInfoDto findWoomsDetail(CustomUserDetails currentUser, Long woomsId) {
+        UUID currentUserUuid = UUID.fromString(currentUser.getUuid());
+
+        Enrollment enrollment = enrollmentRepository.findByPkUserUuidAndWoomsId(currentUserUuid, woomsId)
+                .orElseThrow(WoomsUserNotEnrolledException::new);
+
+        if (!EnrollmentStatus.ACCEPT.equals(enrollment.getStatus())) {
+            throw new WoomsUserNotEnrolledException();
+        }
+
+        List<UserInfoDto> userInfoDtos = enrollmentRepository.findByPkWoomIdAndStatus(woomsId, EnrollmentStatus.ACCEPT).stream()
+                .map(Enrollment::getUser)
+                .map(User::toDto)
+                .collect(Collectors.toList());
+
+        Wooms wooms = woomsRepository.findById(woomsId)
+                .orElseThrow(WoomsNotValidInviteCodeException::new);
+
+        WoomsDto woomsDto = wooms.toDto();
+        return new WoomsDetailInfoDto(woomsDto, userInfoDtos);
+    }
+
+    private User fetchUser(String userUuidStr) {
+        UUID userUuid = UUID.fromString(userUuidStr);
+        return userRepository.findById(userUuid)
+                .orElseThrow(UserNotFoundException::new);
     }
 
     private Wooms findWoomsByInviteCode(String woomsInviteCode) {
