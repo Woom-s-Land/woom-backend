10009eaed24958b3cfbba601f6af4f5b75483166
Songdoeon <ehdjs9583@naver.com>
2024-07-31 10:33:00 +0900

    feat : repository 생성

---
+++ src/main/java/com/ee06/wooms/domain/comments/repository/CommentRepository.java
@@ -0,0 +1,18 @@
+package com.ee06.wooms.domain.comments.repository;
+
+import com.ee06.wooms.domain.comments.entity.Comment;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByWoomsId(Long woomsId, Pageable pageable);
+
+    @Query("SELECT COUNT(c) > 0 FROM Comment c WHERE c.createdDate >= :today AND c.wooms.id = :woomsId AND c.user.uuid = :userUuid")
+    boolean isWriteByToday(@Param("today") LocalDateTime today, @Param("woomsId")Long woomsId, @Param("userUuid")UUID userUuid);
+}
