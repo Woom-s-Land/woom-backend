6b62df64d2a09357d39a57507960d1dcdc8618ae
Songdoeon <ehdjs9583@naver.com>
2024-07-31 10:32:42 +0900

    feat : 예외 생성

---
+++ src/main/java/com/ee06/wooms/domain/comments/exception/CommentExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.ee06.wooms.domain.comments.exception;
+
+import com.ee06.wooms.domain.comments.exception.ex.ExistWroteCommentException;
+import com.ee06.wooms.domain.wooms.exception.ex.WoomsAlreadyMemberException;
+import com.ee06.wooms.global.exception.ErrorCode;
+import com.ee06.wooms.global.util.ErrorCodeUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackages = "com.ee06.wooms.domain.comments")
+public class CommentExceptionHandler {
+    @ExceptionHandler(ExistWroteCommentException.class)
+    public ResponseEntity<Object> existWroteCommentException(WoomsAlreadyMemberException e) {
+        return ErrorCodeUtils.build(ErrorCode.EXIST_WROTE_COMMENT);
+    }
+}
