53a596a2a3668c0d065e69c2cf122ae4bf821179
현수 <znight1020@naver.com>
2024-08-02 14:20:07 +0900

    Feat: 서블릿 Cors, Cookie SameSite 설정 (#81)

---
+++ src/main/java/com/ee06/wooms/global/config/filter/CorsFilter.java
@@ -0,0 +1,23 @@
+package com.ee06.wooms.global.config.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        filterChain.doFilter(request, response);
+
+    }
+}
