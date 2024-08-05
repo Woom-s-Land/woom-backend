f2605169d29839590a1ecf0b91df2fcee2809a02
이현수 <znight1020@naver.com>
2024-08-05 10:01:17 +0900

    Feat : 속해있지 않은 wooms 사용자 예외 처리

--- src/main/java/com/ee06/wooms/domain/stories/service/StoryService.java
+++ src/main/java/com/ee06/wooms/domain/stories/service/StoryService.java
@@ -1,5 +1,6 @@
 package com.ee06.wooms.domain.stories.service;
 
+import com.ee06.wooms.domain.enrollments.repository.EnrollmentRepository;
 import com.ee06.wooms.domain.stories.Script;
 import com.ee06.wooms.domain.stories.dto.StoryResponse;
 import com.ee06.wooms.domain.stories.dto.StoryWriteRequest;
@@ -11,10 +12,10 @@ import com.ee06.wooms.domain.users.exception.ex.UserNotFoundException;
 import com.ee06.wooms.domain.users.repository.UserRepository;
 import com.ee06.wooms.domain.wooms.entity.Wooms;
 import com.ee06.wooms.domain.wooms.exception.ex.WoomsNotValidException;
+import com.ee06.wooms.domain.wooms.exception.ex.WoomsUserNotEnrolledException;
 import com.ee06.wooms.domain.wooms.repository.WoomsRepository;
 import com.ee06.wooms.global.ai.exception.FailedRequestToGptException;
 import com.ee06.wooms.global.ai.service.AIService;
-import com.ee06.wooms.global.aws.FileFormat;
 import com.ee06.wooms.global.aws.service.S3Service;
 import com.ee06.wooms.global.common.CommonResponse;
 import lombok.RequiredArgsConstructor;
@@ -28,7 +29,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static com.ee06.wooms.global.aws.FileFormat.*;
+import static com.ee06.wooms.global.aws.FileFormat.AUDIO_TYPE;
+import static com.ee06.wooms.global.aws.FileFormat.STORY_EXTENSION;
 
 @Service
 @Transactional
@@ -37,6 +39,8 @@ import static com.ee06.wooms.global.aws.FileFormat.*;
 public class StoryService {
     private final AIService aiService;
     private final S3Service s3Service;
+    private final EnrollmentRepository enrollmentRepository;
+
     private final UserRepository userRepository;
     private final StoryRepository storyRepository;
     private final WoomsRepository woomsRepository;
@@ -70,8 +74,10 @@ public class StoryService {
 
     public CommonResponse writeStory(CustomUserDetails userDetails, StoryWriteRequest storyWriteRequest, Long woomsId) {
         String fileName = String.valueOf(UUID.randomUUID());
+
         User user = userRepository.findById(UUID.fromString(userDetails.getUuid())).orElseThrow(UserNotFoundException::new);
         Wooms wooms = woomsRepository.findById(woomsId).orElseThrow(WoomsNotValidException::new);
+        if(!enrollmentRepository.existsByPkUserUuidAndPkWoomId(user.getUuid(), woomsId)) throw new WoomsUserNotEnrolledException();
 
         String script =
                 Optional.ofNullable(aiService.convertScript(storyWriteRequest.getContent(), storyWriteRequest.getUserNickname()))
