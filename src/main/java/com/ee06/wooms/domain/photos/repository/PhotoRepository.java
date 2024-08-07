713b983e39cee0c11e70a14e0385d4ecbbc0679a
Songdoeon <ehdjs9583@naver.com>
2024-08-07 11:25:39 +0900

    feat : map controller 및 repository 추가

--- src/main/java/com/ee06/wooms/domain/photos/repository/PhotoRepository.java
+++ src/main/java/com/ee06/wooms/domain/photos/repository/PhotoRepository.java
@@ -1,5 +1,6 @@
 package com.ee06.wooms.domain.photos.repository;
 
+import com.ee06.wooms.domain.photos.dto.MapResponse;
 import com.ee06.wooms.domain.photos.entity.Photo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,8 +14,6 @@ import java.util.List;
 
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
 
-    List<Photo> findAllByWoomsId(Long woomsId, Pageable pageable);
-
     @Query("SELECT p FROM Photo p WHERE p.createdDate >= :startDate AND p.createdDate < :endDate")
     Page<Photo> findAllByMonth(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, Pageable pageable);
 
@@ -24,4 +23,8 @@ public interface PhotoRepository extends JpaRepository<Photo, Long> {
             "HAVING p2.createdDate = MAX(p2.createdDate)) " + // 각 그룹의 가장 최근 사진 선택
             "ORDER BY p.createdDate DESC")
     Page<Photo> findLatestPhotosByMonth(Pageable pageable);
+
+    @Query("SELECT new com.ee06.wooms.domain.photos.dto.MapResponse(p.mapId, COUNT(p)) " +
+            "FROM Photo p GROUP BY p.mapId")
+    List<MapResponse> findPhotoCounts();
 }
