d902b6d3d9716da652b52fd409994477ed9d0108
홍성우 <sungwoo166@gmail.com>
2024-08-08 17:08:21 +0900

    Feat : Wooms 페이징 구현 (#111)

--- src/main/java/com/ee06/wooms/domain/wooms/repository/WoomsRepository.java
+++ src/main/java/com/ee06/wooms/domain/wooms/repository/WoomsRepository.java
@@ -1,7 +1,9 @@
 package com.ee06.wooms.domain.wooms.repository;
 
 import com.ee06.wooms.domain.wooms.entity.Wooms;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,9 +11,9 @@ import java.util.UUID;
 
 public interface WoomsRepository  extends JpaRepository<Wooms, Long> {
     List<Wooms> findByUserUuid(UUID userUuid);
+    Page<Wooms> findByUserUuid(UUID userUuid, Pageable pageable);
     Optional<Wooms> findByInviteCode(UUID inviteCode);
     Optional<Wooms> findWoomsById(Long woomsId);
-
     boolean existsByUserUuidAndId(UUID userUuid, Long id);
     boolean existsUserUuidByUserUuidAndId(UUID userUuid, Long id);
 }
