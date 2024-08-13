a3af69ed476dbd3ddb2b3c426186b2f13ec5d679
Songdoeon <ehdjs9583@naver.com>
2024-08-13 12:16:46 +0900

    Refactor : contoller 변경

--- src/main/java/com/ee06/wooms/domain/chat/controller/ChatController.java
+++ src/main/java/com/ee06/wooms/domain/chat/controller/ChatController.java
@@ -3,23 +3,18 @@ package com.ee06.wooms.domain.chat.controller;
 import com.ee06.wooms.domain.chat.ChannelRepository;
 import com.ee06.wooms.domain.chat.SessionRepository;
 import com.ee06.wooms.domain.chat.dto.ChatMessage;
-import com.ee06.wooms.domain.chat.dto.MoveRequest;
-import com.ee06.wooms.domain.chat.dto.MoveResponse;
+import com.ee06.wooms.domain.chat.dto.MoveMessage;
 import com.ee06.wooms.domain.chat.entity.Channel;
 import com.ee06.wooms.domain.chat.entity.Woom;
-import com.ee06.wooms.domain.users.dto.CustomUserDetails;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.UUID;
@@ -35,32 +30,25 @@ public class ChatController {
     private final SessionRepository sessionRepository;
 
     @PostMapping("/api/join/{woomsId}")
-    public void join(@DestinationVariable("woomsId") UUID woomsId,
-                     HttpServletRequest request) {
-        Woom woom = new Woom(getDetail(), woomsId);
+    public void join(@PathVariable("woomsId") UUID woomsId) {
         Channel channel = channelRepository.get(woomsId);
         channel.getWooms().forEach(woom1 ->
                 {
                     try {
-                        log.info("woom : {} ", MoveResponse.of(woom1));
-                        template.convertAndSend("/move/" + woomsId, objectMapper.writeValueAsString(MoveResponse.of(woom1)));
+                        log.info("woom : {} ", MoveMessage.of(woom1));
+                        template.convertAndSend("/move/" + woomsId, objectMapper.writeValueAsString(MoveMessage.of(woom1)));
                     } catch (JsonProcessingException e) {
                         throw new RuntimeException(e);
                     }
                 }
         );
-        sessionRepository.put(request.getSession().getId(), woomsId);
-        channel.addWoom(woom);
-        channelRepository.put(woomsId, channel);
     }
 
     @MessageMapping("/chat/{woomsId}")
     public void sendMessage(@DestinationVariable("woomsId") UUID woomsId,
                             String content) throws Exception {
-        ChatMessage chatMessage = ChatMessage.builder()
-                .content(content)
-                .nickname(getDetail().getNickname())
-                .build();
+        ChatMessage chatMessage = objectMapper.readValue(content, ChatMessage.class);
+
         log.info("chatMessage: {}", chatMessage);
         template.convertAndSend("/chat/" + woomsId, objectMapper.writeValueAsString(chatMessage));
     }
@@ -68,30 +56,11 @@ public class ChatController {
     @MessageMapping("/move/{woomsId}")
     public void move(@DestinationVariable("woomsId") UUID woomsId,
                      String content) throws Exception {
-        CustomUserDetails userDetails = getDetail();
-        log.info("content {}", content);
-        MoveRequest moveRequest = objectMapper.readValue(content, MoveRequest.class);
-        MoveResponse moveResponse = getMoveResponse(userDetails, moveRequest);
+        MoveMessage moveMessage = objectMapper.readValue(content, MoveMessage.class);
 
-        channelRepository.get(woomsId).moveWoom(new Woom(userDetails, woomsId), moveRequest);
-        log.info("moveRequest: {}", moveRequest);
-        log.info("moveResponse: {}", moveResponse);
-        template.convertAndSend("/move/" + woomsId, objectMapper.writeValueAsString(moveRequest));
+        channelRepository.get(woomsId).moveWoom(new Woom(moveMessage.getNickname(), moveMessage.getCostume(), woomsId), moveMessage);
+        log.info("moveMessage: {}", moveMessage);
+        template.convertAndSend("/move/" + woomsId, objectMapper.writeValueAsString(moveMessage));
     }
 
-    public MoveResponse getMoveResponse(CustomUserDetails userDetails, MoveRequest moveRequest) {
-        return MoveResponse.builder()
-                .x(moveRequest.getX())
-                .y(moveRequest.getY())
-                .nickName(userDetails.getNickname())
-                .costume(Integer.parseInt(userDetails.getCostume()))
-                .stepId(moveRequest.getStepId())
-                .direction(moveRequest.getDirection())
-                .build();
-    }
-
-    public CustomUserDetails getDetail(){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return (CustomUserDetails) authentication.getDetails();
-    }
 }
