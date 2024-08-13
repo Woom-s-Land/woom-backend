8ce65302389287d7a244563a1144d950b9e2520e
Songdoeon <ehdjs9583@naver.com>
2024-08-13 12:15:25 +0900

    Feat : 자원관리 entity 추가

--- src/main/java/com/ee06/wooms/domain/chat/entity/Woom.java
+++ src/main/java/com/ee06/wooms/domain/chat/entity/Woom.java
@@ -1,7 +1,6 @@
 package com.ee06.wooms.domain.chat.entity;
 
-import com.ee06.wooms.domain.chat.dto.MoveRequest;
-import com.ee06.wooms.domain.users.dto.CustomUserDetails;
+import com.ee06.wooms.domain.chat.dto.MoveMessage;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,48 +16,41 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Woom {
-    private UUID id;
-    private String username;
+    private String nickname;
     private UUID woomsId;
     private Integer costume;
-    private Integer x;
-    private Integer y;
+    private String x;
+    private String y;
     private Integer direction;
     private Integer stepId;
 
-    public Woom(CustomUserDetails userDetails, UUID woomsId){
-        this.id = UUID.fromString(userDetails.getUuid());
-        this.username = userDetails.getUsername();
+    public Woom(String nickname, Integer costume,  UUID woomsId){
+        this.nickname = nickname;
         this.woomsId = woomsId;
-        this.costume = Integer.valueOf(userDetails.getCostume());
-        this.x = 1020;
-        this.y = 750;
+        this.costume = costume;
+        this.x = "1020";
+        this.y = "750";
         this.direction = 0;
     }
 
-    public Woom move(MoveRequest moveRequest){
-        this.x = moveRequest.getX();
-        this.y = moveRequest.getY();
-        this.direction = moveRequest.getDirection();
-        this.stepId = moveRequest.getStepId();
+    public Woom move(MoveMessage moveMessage){
+        this.x = moveMessage.getX();
+        this.y = moveMessage.getY();
+        this.direction = moveMessage.getDirection();
+        this.stepId = moveMessage.getStepId();
 
         return this;
     }
-
-    public static Woom deleteWoom(UUID id){
-        return Woom.builder().id(id).build();
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Woom woom = (Woom) o;
-        return Objects.equals(id, woom.id);
+        return Objects.equals(nickname, woom.nickname) && Objects.equals(woomsId, woom.woomsId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(id);
+        return Objects.hash(nickname, woomsId);
     }
 }
